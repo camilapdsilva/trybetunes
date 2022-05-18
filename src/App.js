e28015ed17941499afe9e218896e9092e8229eae
Mila Souza <milinhaps27@gmail.com>
2022-05-18 15:46:09 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+
+// import { Album, Favorites, Login, NotFound, Profile, ProfileEdit, Search } from './pages';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
+      </BrowserRouter>
+
+    );
   }
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
